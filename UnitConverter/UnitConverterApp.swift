45e0b05ef770d12a53c67afe97c1d9b08d4d7bff
Gennady Raster <raster.iosdev@gmail.com>
2021-12-02 20:26:28 +0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Gena Raster on 2.12.21.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
