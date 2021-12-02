a99ee2b51d151219591bdaeaac5042274ffbb36f
Gennady Raster <raster.iosdev@gmail.com>
2021-12-02 23:51:18 +0300

    added enum of main items, picker and text field.

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum Items: String, CaseIterable {
+    case lenghth, temp, volume
+    }
+    
+    func getItemsArray () -> [String] {
+        var arr = [String]()
+        for item in Items.allCases {
+            arr.append(item.rawValue)
+        }
+        return arr
+    }
+    
+    @State private var choosenItem = Items.temp.rawValue
+    
+    @State private var incomingValue: Double = 0
+    
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                
+                Section (header: Text("Choose some unit:")) {
+                    Picker("Items", selection: $choosenItem) {
+                        ForEach(getItemsArray(), id: \.self) {
+                            Text($0.uppercased())
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                HStack {
+                    TextField("Value", value: $incomingValue,
+                              format: .number,
+                              prompt: Text("Enter a value"))
+                        .padding()
+                }
+                
+                Spacer()
+    
+            }
             .padding()
+            .navigationTitle("Unit Converter")
+            
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
